0bf8c832fc7b9ea3fad0465c7db2ca468d8608e4
Jamie Foster <jamiefoster15@hotmail.co.uk>
2021-06-20 14:00:21 +0100

    added combo box and check boxes

--- FrameContents.java
+++ FrameContents.java
@@ -3,44 +3,89 @@ import java.awt.Font;
 import javax.swing.*;
 
 public class FrameContents extends JPanel {
-
 	
 	public final int MIDDLE = 200;
+	public final int DEFAULTPASSWORDLENGTH = 16;
 	
-	JLabel title = new JLabel("Generate Password");
-	
+	JLabel title = new JLabel("Generate Password");	
+	JLabel lengthPrompt = new JLabel("Choose length: ");
+	JLabel includeUppercaseLettersPrompt = new JLabel("Include uppercase Letters: ");
+	JLabel includeLowercaseLettersPrompt = new JLabel("Include lowercase Letters: ");
+
+
 	JTextField passwordContainer = new JTextField();
 	
-	JComboBox<Integer> passwordLength = new JComboBox<Integer>();
+	JComboBox<Integer> passwordLengthBox = new JComboBox<Integer>();
 	
-	int[] a = new int[10];
+	JCheckBox includeUppercaseLetters = new JCheckBox();
 	
+	JCheckBox includeLowercaseLetters = new JCheckBox();
+	
+	JCheckBox includeSymbols = new JCheckBox();
+
 	public FrameContents() {
-		setFrameContents();
+		this.setLayout(null);
+		setPanelContents();
 		addComboBoxItems();
 	} 
 	
 	public void addComboBoxItems() {
 		
-		for (int i = 0; i < 10; i++) {
-			passwordLength.addItem(i);
-			a[i] = i;
+		for (int i = 1; i < 21; i++) {
+			passwordLengthBox.addItem(i);
 		}
 		
-		passwordContainer.setText(Integer.toString(a.length));
+		passwordLengthBox.setSelectedItem(DEFAULTPASSWORDLENGTH);
 	}
 	
-	public void setFrameContents() {
+	//sets the contents of the panel 
+	public void setPanelContents() {
+	
+		setFonts();
 		
-		this.setLayout(null);
-		title.setVisible(true);
-		Font font = new Font ("Arial Black", Font.BOLD, 20);
-		title.setFont(font);
-		this.add(title);
+		addItemsToPanel();
 		
+		setItemBounds();
+	}
+	
+	
+	void setFonts() {
+		
+		Font headingFont = new Font ("Arial Black", Font.BOLD, 20);
+		Font bodyFont = new Font ("Arial Black", Font.PLAIN, 12);
+		
+		title.setFont(headingFont);
+		lengthPrompt.setFont(bodyFont);		
+		includeUppercaseLettersPrompt.setFont(bodyFont);
+		includeLowercaseLettersPrompt.setFont(bodyFont);
+	}
+	
+	void addItemsToPanel() {
+		
+		this.add(title);	
 		this.add(passwordContainer);
+		this.add(passwordLengthBox);
+		this.add(lengthPrompt);
+		this.add(includeUppercaseLetters);
+		this.add(includeUppercaseLettersPrompt);
+		this.add(includeLowercaseLetters);
+		this.add(includeLowercaseLettersPrompt);
+	}
+	
+	void setItemBounds(){
 		
 		title.setBounds(30, 1, 1000, 100);
-		passwordContainer.setBounds(30, 300, 200, 30);
+		
+		passwordContainer.setBounds(30, 300, 230, 30);
+		
+		passwordLengthBox.setBounds(150, 100, 100, 30);
+		lengthPrompt.setBounds(35, 100, 1000, 30);	
+		
+		includeUppercaseLetters.setBounds(210, 145, 20, 20);
+		includeUppercaseLettersPrompt.setBounds(35, 130, 1000, 50);
+		
+		includeLowercaseLetters.setBounds(210, 175, 20, 20);
+		includeLowercaseLettersPrompt.setBounds(35, 135, 1000, 100);
+		
 	}
 }
